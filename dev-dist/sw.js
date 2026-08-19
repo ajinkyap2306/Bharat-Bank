99c4eb19d24bb97a6e3c39d35e7e8ac36074cdb6
Deepak Yadav <deepakkumar.yadav@npstx.com>
2026-08-19 15:54:46 +0530

    added

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.6079p0olbh8"
+    "revision": "0.1crb45ls038"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
